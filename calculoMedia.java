b51c36da38f693cfe46e6434b17bb95df56bda27
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-06-08 17:41:27 +0000

    OK

---
+++ calculoMedia.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CalculoMedia{
+    
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a nota do primeiro trimestre: ")
+        double nota1 = scanner.nextDouble();
+
+        double nota2 = scanner.nextDouble();
+        
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+    
+    
+        System.out.printf("A média final é: ", media);
+
+        if(media < 4.0){
+            System.out.println("Reprovado");
+        } else if(media <= 6.0) {
+            System.out.println("Se liga");
+        } else {
+            System.out.println("Aprovado");
+        }
+    
+    
+    
+    }
+}
